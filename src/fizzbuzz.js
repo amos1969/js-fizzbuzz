6dfdbc24ad8289dd61f198038d95120cd778dfa4
Dave Ames <david.john.ames@gmail.com>
2023-04-11 09:45:45 +0100

    Solution to problem

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,19 @@
 // TODO: Write your function in this file
+function fizzbuzz(lower, upper) {
+  const theArray = []
+  for (let i = lower; i <= upper; i++) {
+    if (i % 15 === 0) {
+      theArray.push('FizzBuzz')
+    } else if (i % 5 === 0) {
+      theArray.push('Buzz')
+    } else if (i % 3 === 0) {
+      theArray.push('Fizz')
+    } else {
+      theArray.push(i)
+    }
+  }
+  return theArray
+}
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzbuzz
